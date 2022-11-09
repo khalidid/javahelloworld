1da3d39f6292060ea5a26c22998e8c7fc7ec4115
khalidid <35107809+khalidid@users.noreply.github.com>
2022-11-09 23:40:48 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, khalid*******!");
+        System.out.println("Hello, khalid- Trigger");
     }
 }
